5463e27abf6c79ec7824bc0983b943f768586384
avikam <avikam@gmail.com>
2021-08-03 07:52:18 -0400

    liberate some of the redundency

--- src/main/java/DataMonitor.java
+++ src/main/java/DataMonitor.java
@@ -190,8 +190,6 @@ public class DataMonitor {
 
                 currentPath = "";
                 candidateWorker = null;
-
-                clusterConfFinder.Find();
                 return;
             }
         }
